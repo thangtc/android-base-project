a11c71a66033a34866aa5d7cfbb46da069a50585
Thang Truong <tcthang.dsna@gmail.com>
2015-09-25 14:45:46 +0700

    setup circleci

---
+++ BaseProject/app/src/main/java/tct/com/baseproject/AppController.java
@@ -0,0 +1,10 @@
+package tct.com.baseproject;
+
+import android.app.Application;
+
+/**
+ * Copyright © 2015 AsianTech inc.
+ * Created by Thang Truong on 9/25/15.
+ */
+public class AppController extends Application {
+}
